bd57ef994220a5fd836b7d595dd7e992cf910c8b
Patrick Bourget <patrickmbourget@gmail.com>
2020-01-21 20:42:36 -0500

    with a little more style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,9 @@
 class QuestionsController < ApplicationController
   def ask
-    # @ask = params[:question]
-    # raise
-    # @question = params[:question]
   end
 
   def answer
     @question = params[:question]
-
-    # unless @question == "I am going to work"
-    #   return @answer = "I don't care, get dressed and go to work!"
-    # else @question.include? ("?")
-    #   return @answer = "Silly question, get dressed and go to work!"
-    # else
-    #   return @answer = "Great"
-    # end
 
     if @question != "I am going to work" && @question.exclude?("?")
       return @answer = "I don't care, get dressed and go to work!"
